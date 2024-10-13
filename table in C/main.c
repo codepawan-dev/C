6bc7174e1661532decebf38035b4c00771212064
codepawan-dev <pawansdevadiga1@gmail.com>
2024-10-13 10:04:36 +0530

    main.c

--- table in C/main.c
+++ table in C/main.c
@@ -3,15 +3,39 @@
 
 int main()
 {
+    int n;
     system("cls");
-    int table;
-    printf("Enter a number: ");
-    scanf("%d", &table);
-
-    for (int i = 1; i <= 10; i++)
+    printf("Enter the number how many table you want: ");
+    scanf("%d", &n);
+    int mul[n];
+    for (int i = 0; i < n; i++)
     {
-        printf("%d x %d = %d\n", table, i, table * i);
+        printf("Enter table: ");
+        scanf("%d", &mul[i]);
     }
 
+    int arr[n][10];
+
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            arr[i][j] = mul[i] * (j + 1);
+        }
+    }
+
+
+    for (int i = 0; i < n; i++)
+    {
+        printf("\nThe table of %d\n", mul[i]);
+        for (int j = 0; j < 10; j++)
+        {
+            printf("%d x %d = %d\n", mul[i], j + 1, arr[i][j]);
+        }
+    }
+
+    getchar();
+    printf("\nPress Enter key to exit");
+    getchar();
     return 0;
 }
