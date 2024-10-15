9fc48d3d81df4b9c04f9231dd3ff61f0d8856053
codepawan-dev <pawansdevadiga1@gmail.com>
2024-10-15 21:54:52 +0530

    main.c

--- table in C/main.c
+++ table in C/main.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
-#include <stdlib.h>
 
 int main()
 {
     int n;
-    system("cls"); // for windows
-    // system("clear"); // for uninux based operating system 
+    FILE *fptr;
+    system("cls"); // For Windows
+    // system("clear"); // For UNIX based operating system
     printf("Enter the number how many table you want: ");
+
     scanf("%d", &n);
     int mul[n];
     for (int i = 0; i < n; i++)
@@ -25,18 +26,24 @@ int main()
         }
     }
 
-
+    fptr = fopen("table.txt", "w");
     for (int i = 0; i < n; i++)
     {
         printf("\nThe table of %d\n", mul[i]);
+        fprintf(fptr, "The table of %d\n", mul[i]);
         for (int j = 0; j < 10; j++)
         {
             printf("%d x %d = %d\n", mul[i], j + 1, arr[i][j]);
+            fprintf(fptr, "%d x %d = %d\n", mul[i], j + 1, arr[i][j]);
         }
+        fprintf(fptr, "\n");
     }
+
+    fclose(fptr);
 
     getchar();
     printf("\nPress Enter key to exit");
     getchar();
+
     return 0;
 }
